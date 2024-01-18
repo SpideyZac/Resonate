eeb2ddd772386ea329f1ade6cd21e7403915cbb0
Aarvin R <144612896+aarvinr@users.noreply.github.com>
2024-01-18 12:38:00 -0800

    feat: mp3 structure
    - based on `src/format/wav.rs`

--- src/format/mp3.rs
+++ src/format/mp3.rs
@@ -1,3 +1,27 @@
-struct MP3Format {
+use std::fs::File;
 
+struct MP3Format {
+    format: u8,
+    channels: u8,
+    sample_rate: u16,
+    bits_per_sample: u8,
+    data: Vec<u8>,
+}
+
+impl MP3Format {
+    fn decode(file: File) -> MP3Format {
+        //read file and convert to mp3 format
+        MP3Format {
+            format: 2,
+            channels: 2,
+            sample_rate: 44100,
+            bits_per_sample: 16,
+            data: Vec::new(),
+        }
+    }
+
+    fn encode(path: &str) -> File {
+        //convert mp3 format to file
+        File::create(path).unwrap()
+    }
 }
